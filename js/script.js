0023f0cd901df32610c15284d1f449be680cc33f
Uktam <uktam.khamidov@stanum.us>
2021-01-17 15:53:28 +0500

    preloader percentage

--- js/script.js
+++ js/script.js
@@ -17,8 +17,27 @@ $(document).ready(function () {
   });
 });
 
+let
+  images = document.images,
+  images_total_count = images.length,
+  images_loaded_count = 0,
+  perc_display = document.getElementById('load_perc');
+const image_loaded = () => {
+  images_loaded_count++;
+  perc_display.innerHTML = (((100 / images_total_count) * images_loaded_count) << 0)
+  if (images_loaded_count >= images_total_count) {
+    closePreloader();
+  }
+}
 
-document.body.onload = () => {
+for (let i = 0; i < images_total_count; i++) {
+  image_clone = new Image();
+  image_clone.onload = image_loaded;
+  image_clone.onerror = image_loaded;
+  image_clone.src = images[i].src;
+}
+
+const closePreloader = () => {
   const preloader = document.querySelector('.preloader');
   setTimeout(() => {
     preloader.style.opacity = '0'
